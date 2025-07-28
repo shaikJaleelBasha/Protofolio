4037096ce8f955bb65b67a657a0d192c04ce2834
shaikJaleelBasha <jaleelbashashaik18@gmail.com>
2025-07-29 05:10:59 +0530

    app file updated1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Education from "./components/Education";
-<<<<<<< HEAD
+
 import Skills from "./components/Skills";
 import Protofolio from "./components/Protofolio";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
-=======
-import Portfolio from "./components/Portfolio";
-import Experience from "./components/Experience";
->>>>>>> 16f971b ( all files updated)
+
 import Contact from "./components/Contact";
 
 function App() {
   return (
-<<<<<<< HEAD
+
     <>
       <div>
         <Navbar />
@@ -27,17 +24,7 @@ function App() {
         <Footer />
       </div>
     </>
-=======
-    <div>
-      <Navbar />
-      <section id="home"><Home /></section>
-      <section id="education"><Education /></section>
-      <section id="projects"><Portfolio /></section>
-      <section id="experience"><Experience /></section>
-      <section id="contact"><Contact /></section>
-    </div>
->>>>>>> 16f971b ( all files updated)
-  );
+  )
 }
 
 export default App;
