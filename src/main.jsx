e2c020fa8af659e8a6e27255f232cb6adc358667
shaikJaleelBasha <jaleelbashashaik18@gmail.com>
2025-07-29 05:06:58 +0530

    main file updated1

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -9,15 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> 16f971b ( all files updated)
+
